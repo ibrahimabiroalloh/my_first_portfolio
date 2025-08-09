774fe67778fd63ed226d64c51cb8217ce6f2c6e3
Ibrahim Biro Diallo <dibrahimabiro@gmail.com>
2025-08-09 14:54:43 +0000

    Initial commit of portfolio project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ==================== toggle style switcher ====================*/
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","web Developer","IT Helper","Front-End Developer"],
+    typedSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
